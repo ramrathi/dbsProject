2177d72414a90ff5a62b7317482305940209502d
ramrathi <ryrathi@gmail.com>
2019-10-14 00:04:30 +0530

    Added sessions and logout

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import mysql.connector
 from os.path import join, dirname
 from dotenv import load_dotenv
 from flask import request,session,redirect
+from flask import *
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -23,9 +24,13 @@ cursor = mydb.cursor()
 from flask import Flask, render_template
 app = Flask(__name__)
 
+app.secret_key = "fuck off"
+
 @app.route('/',methods=['GET'])
 def home():
-	return render_template("./home.html")
+	if 'logged_in' in session: 
+		return render_template("./home.html")
+	else :return redirect('/login')
 
 @app.route('/login',methods=['GET','POST'])
 def login():
@@ -37,11 +42,15 @@ def login():
 		data = cursor.fetchall()
 		print(data)
 		if len(data)==0:
+			print("Wrong password")
 			return render_template('./index.html',error = "Wrong email or password")
 		else:
 			# Set session variables to true
+			session['logged_in'] = True
 			return redirect('/')
 	else:
+		if 'logged_in' in session:
+			return redirect('/')
 		return render_template("./index.html")
 
 @app.route('/register',methods=['GET'])
@@ -51,6 +60,11 @@ def register():
 @app.route('/account',methods=['GET'])
 def account():
 	return render_template("./profile.html")
+
+@app.route('/logout',methods=['POST'])
+def logout():
+	session.clear()
+	return redirect('/login')
 
 
 if __name__ == "__main__":
